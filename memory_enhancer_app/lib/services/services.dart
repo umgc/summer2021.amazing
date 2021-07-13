9faa5b4566b799cadaa6b477dc93d62e7d8d000d
aydot4life2009 <aydot4life@yahoo.com>
2021-07-12 20:16:43 -0400

    Encryption implementation
    Encryption implementation and unit test

--- memory_enhancer_app/lib/services/services.dart
+++ memory_enhancer_app/lib/services/services.dart
@@ -4,6 +4,7 @@
 //**************************************************************
 import 'package:memory_enhancer_app/app/app_router.gr.dart';
 import 'package:memory_enhancer_app/services/speech/speech_service.dart';
+import 'package:memory_enhancer_app/services/encryption/encryption_service.dart';
 
 import 'get_it.dart';
 
@@ -13,4 +14,9 @@ final appRouter = AppRouter();
 // The speech service (Singleton)
 SpeechService get speechService {
   return getIt.get<SpeechService>();
+}
+
+// The encryption/decryption service (Singleton)
+EncryptionService get encryptionService {
+  return getIt.get<EncryptionService>();
 }
