f4434f100f71e17f8c07ed834b00b3b3be4d220e
c-ahmed <christian.ahmed@gmail.com>
2021-08-02 22:19:56 -0400

    Implemented the icons and splash screens for Android and iOS platform

--- memory_enhancer_app/android/app/src/main/java/com/teamamazing/memory_enhancer_app/MainActivity.java
+++ memory_enhancer_app/android/app/src/main/java/com/teamamazing/memory_enhancer_app/MainActivity.java
@@ -1,50 +1,6 @@
 package com.teamamazing.memory_enhancer_app;
 
-/*
- * IBM Speech to Text and Text to Speech SDK use.
- * Author: Christian Ahmed
- */
-
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.widget.Toast;
-
-import com.google.gson.Gson;
-import com.ibm.cloud.sdk.core.http.HttpMediaType;
-import com.ibm.cloud.sdk.core.security.Authenticator;
-import com.ibm.cloud.sdk.core.security.IamAuthenticator;
-import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneHelper;
-import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
-import com.ibm.watson.developer_cloud.android.library.audio.StreamPlayer;
-import com.ibm.watson.developer_cloud.android.library.audio.utils.ContentType;
-import com.ibm.watson.speech_to_text.v1.SpeechToText;
-import com.ibm.watson.speech_to_text.v1.model.RecognizeOptions;
-import com.ibm.watson.speech_to_text.v1.model.RecognizeWithWebsocketsOptions;
-import com.ibm.watson.speech_to_text.v1.model.SpeakerLabelsResult;
-import com.ibm.watson.speech_to_text.v1.model.SpeechRecognitionResults;
-import com.ibm.watson.speech_to_text.v1.model.SpeechTimestamp;
-import com.ibm.watson.speech_to_text.v1.websocket.BaseRecognizeCallback;
-import com.ibm.watson.speech_to_text.v1.websocket.RecognizeCallback;
-import com.ibm.watson.text_to_speech.v1.TextToSpeech;
-import com.ibm.watson.text_to_speech.v1.model.SynthesizeOptions;
-
-import java.io.InputStream;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-// For Channel creation ----------------------------------------------------------------------------
-import androidx.annotation.NonNull;
-
-import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
-
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
 
